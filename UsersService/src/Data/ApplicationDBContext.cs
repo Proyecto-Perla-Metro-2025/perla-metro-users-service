a694d8f98aa82de0cb22fca55ec1c1a23eb3f563
towcrash <fernando.vega@alumnos.ucn.cl>
2025-09-20 23:09:20 -0300

    feat: create User model and DbContext

---
+++ UsersService/src/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UsersService.src.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        { }
+
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ UsersService/src/Model/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersService.src.Model
+{
+    public class User
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string Surename { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public DateOnly RegistrationDate { get; set; }
+        public bool State { get; set; }
+    }
+}
